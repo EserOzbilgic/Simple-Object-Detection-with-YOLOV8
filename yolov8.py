de46f14f46ae85c0c372f6fdf9251db3b3232515
Eser Özbilgiç <120578528+EserOzbilgic@users.noreply.github.com>
2024-08-14 16:25:34 +0300

    Add files via upload

---
+++ yolov8.py
@@ -0,0 +1,38 @@
+import cv2
+import supervision as sv
+from ultralytics import YOLO
+
+model = YOLO('yolov8n.pt')
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Kameradan görüntü gelmiyor...")
+
+bounding_box_annotator = sv.BoundingBoxAnnotator()
+label_annotator = sv.LabelAnnotator()
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    results = model(frame)[0]
+    detections = sv.Detections.from_ultralytics(results)
+
+    annotated_image = bounding_box_annotator.annotate(
+        scene=frame, detections=detections)
+    annotated_image = label_annotator.annotate(
+        scene=annotated_image, detections=detections)
+
+    cv2.imshow("Webcam", annotated_image)
+
+    k = cv2.waitKey(1)
+
+    if k % 256 == 27:
+        print("Esc tuşuna basıldı.. Kapatılıyor..")
+        break
+
+cap.release()
+cv2.destroyAllWindows()
